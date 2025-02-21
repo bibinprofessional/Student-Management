7951caa88d33397f9db5ec3a42d186a429592da8
bibin <bibin.n@dford.co.in>
2025-02-21 22:23:47 +0530

    chore: Module and DocType Creation(Student Enrollment and Courses)

---
+++ student_management/student_management/doctype/student_enrollment/student_enrollment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Bibin N and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student Enrollment", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ student_management/student_management/doctype/student_enrollment/test_student_enrollment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Bibin N and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStudentEnrollment(FrappeTestCase):
+	pass
