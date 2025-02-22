d9b150d88d5b3b03da9adb478aad966c61945c4f
bibin <bibin.n@dford.co.in>
2025-02-22 23:31:11 +0530

    chore: added server validation for email and date

--- student_management/student_management/doctype/student_enrollment/student_enrollment.py
+++ student_management/student_management/doctype/student_enrollment/student_enrollment.py
@@ -3,13 +3,26 @@
 
 import frappe
 from frappe.model.document import Document
-
+from frappe.exceptions import ValidationError
+from datetime import datetime
+from frappe.utils import validate_email_address
 
 class StudentEnrollment(Document):
-	def validate(self):
-		"""Validate required fields"""
-		required_fields = ["student_name", "course", "email"]
+    def validate(self):
+        """Validate required fields, enrollment date, and email format."""
+        
+        # Validate required fields
+        required_fields = ["student_name", "course", "email"]
+        for fieldname in required_fields:
+            if not self.get(fieldname):
+                frappe.throw(f"{frappe.bold(frappe.unscrub(fieldname))} is required")
 
-		for fieldname in required_fields:
-			if not self.get(fieldname):
-				frappe.throw(f"{frappe.bold(frappe.unscrub(fieldname))} is required")
+        # Validate that enrollment date is not in the future
+        if self.enrollment_date:
+            enrollment_date = datetime.strptime(self.enrollment_date, "%Y-%m-%d")
+            if enrollment_date > datetime.today():
+                frappe.throw("Enrollment date cannot be in the future.", ValidationError)
+
+        # Validate email format
+        if self.email and not validate_email_address(self.email):
+            frappe.throw("Please enter a valid email address.", ValidationError)
